b91db58c1278539339d00af335a399b84720f8fd
Brandon Fields <brandonfields10@icloud.com>
2019-06-23 19:08:59 -0400

    game is fully functional, just can't seem to get your score to stay on the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
     var counter = 0;
     var win= 0;
     var loss= 0;
+    
 
-
-    function randomNum(max){
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max +1));
-    };
+    function randomNum(min,max){
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max-min +1)) + min;};
+    // function randomNum(max){
+    //     max = Math.floor(max);
+    //     return Math.floor(Math.random() * (max +1));
+    // };
     // for (var i = 0; i < )
 
     var targetNumber = 77;
@@ -16,62 +20,67 @@ $(document).ready(function(){
 
     var imageone= randomNum(1,10);
         console.log(imageone);
-    var imagetwo= randomNum(20,30);
+    var imagetwo= randomNum(20,3);
         console.log(imagetwo);
-    var imagethree= randomNum(1,20);
+    var imagethree= randomNum(1,21);
         console.log(imagethree);
-    var imagefour= randomNum(25, 2);
+    var imagefour= randomNum(40, 12);
         console.log(imagefour);
 
-    function reset (){
-        counter = 0;
-        $("#h1").html(targetNumber);
-        targetNumber = randomNum(20,80);
-        $(".playerscore").text(counter);
-        imageone= randomNum(1,10);
-        imagetwo= randomNum(20, 30);
-        imagethree= randomNum(20,40);
-        imagefour= randomNum(25, 2);
-    }
+    
 
     $("#spinningd").on("click", function(){
         counter += imageone;
         winsloss();
-        $(".playerscore").text(counter);
+        $("#h3").text(counter);
     });
     
     $("#diamond2").on("click", function(){
         counter += imagetwo;
         winsloss();
-        $(".playerscore").text(counter);
+        $("#h3").text(counter);
     });
    
     $("#nwts").on("click", function(){
         counter += imagethree;
         winsloss();
-        $(".playerscore").text(counter);
+        $("#h3").text(counter);
     });
 
     $("#blackdiamond").on("click", function(){
         counter += imagefour;
         winsloss();
-        $(".playerscore").text(counter);
+        $("#h3").text(counter);
     });
      
     function winsloss (){
 
         if (counter === targetNumber){
-            win += 1;
+            win++;
             alert("Gahh Damn Good Job!!!");
-            $(win).text(win);
+            $("#wins").text(win);
             reset();
         } else if (counter > targetNumber){
-            loss += 1;
+            loss++;
             alert("Pshhh You are sorry try again shawty!");
-            $(loss).text(loss);
+            $("#losses").text(loss);
             reset();
         }
     }
 
+    function reset (){
+        counter = 0;
+        $(".diamond-img").html(targetNumber);
+        targetNumber = randomNum(min,max);
+        $(".playerscore").text(counter);
+        imageone= randomNum(min,max);
+        imagetwo= randomNum(min,max);
+        imagethree= randomNum(min,max);
+        imagefour= randomNum(min,max);
+    }
+
+    $("#reset").on("click", function(){
+        reset();
+    })
 
 });
